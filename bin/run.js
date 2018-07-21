043ba80d70355e8088982be5b3d7e2ae8ae696f1
Jordi Moraleda <moraleda@uniclau.com>
2018-07-21 03:05:03 +0200

    Very basic support for NPM scripts

--- bin/run.js
+++ bin/run.js
@@ -3,11 +3,25 @@
 
 const { detectRunner, digestParameters } = require("../lib/runners")
 const { asyncSpawn } = require("../lib/spawning")
+const { showUsage } = require("../lib/usage")
 
-const runner = detectRunner()
-const args = digestParameters(process.argv, runner)
+function main() {
+  if (process.argv.length == 2) {
+    showUsage()
+    return Promise.resolve()
+  }
 
-asyncSpawn(runner, args).then(result => {
+  let [err, runner] = detectRunner()
+  if (err) {
+    return Promise.reject(err)
+  }
+
+  const args = digestParameters(process.argv, runner)
+
+  return asyncSpawn(runner, args)
+}
+
+main().then(result => {
   process.exit(0)
 }).catch(err => {
   console.error(err.message)
